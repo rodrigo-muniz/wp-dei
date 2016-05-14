4b52d53431ebefefa9a65649e73668cc6c1fcf9d
rodrigo-mamangava <rodrigo@mamangava.com>
2016-05-14 14:07:01 -0300

    inicio do ajuste do MENU
    - install jquery e bootstrap via bower
    - sidebar.php refatorado
    - header.php refatorado
    - style.css alterado
    - function.php, função dei_scripts() recapturado

--- wp-content/themes/dei/sidebar.php
+++ wp-content/themes/dei/sidebar.php
@@ -1,20 +1,3 @@
 <aside class="col-sm-12 col-md-4 col-lg-4">
-	<!-- <div class="accordion dei-accordion"></div> -->
-	<nav class="dei-accordion">
-		<div id="accordion">
-			<?php wp_nav_menu(array('menu' => 'accordion-menu')); ?>	
-		</div>
-		
-	</nav>
-	<div id="sidebar-footer">
-		<div class="social-icons"></div>
-		<div class="info-dei">
-			<span class="puc-footer">Pontifícia Universidade Católica do Rio de Janeiro</span>
-			<p class="logr-footer">Rua Marquês de São Vicente, 225, Gávea<br>
-			Rio de Janeiro, RJ - Brasil<br>
-			Cep: 22451-900 - Cx. Postal: 38097<br>
-			Telefone: +55 (21) 3527 1001</p>
-			<span class="copright-footer">PUC-RIO ©2015 · Todos os direitos reservados.</span>
-		</div>
-	</div>
+    <?php dynamic_sidebar(); ?>	
 </aside>
